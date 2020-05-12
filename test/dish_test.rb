fa0c7365f486ae831c0a51d934209c45d511e20a
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-11 23:15:45 -0600

    Create iteration 1 dish tests

---
+++ test/dish_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+class DishTest < Minitest::Test =
+
+  def setup
+    @dish = Dish.new("Couscous Salad", :appetizer)
+  def test_it_exits
+    assert_instance_of Dish, @dish
+  end
+
+  def test_dish_initialized_with_a_name
+    assert_equal "Couscous Salad", @dish.name
+  end
+
+  def test_dish_initialized_with_a_category
+    assert_equal :appetizer, @dish.category
+  end
+
+end
