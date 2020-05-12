413847c1c9bc1cd7becbb1b34edd80a2b601fced
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-12 00:21:50 -0600

    Modify ratio test to reflect percentages

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -75,8 +75,14 @@ class PotluckTest < Minitest::Test
   end
 
   def test_it_can_give_ratio_of_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
     assert_equal 50.0, @potluck.ratio(:appetizer)
-    assert_equal 0.33, @potluck.ratio(:entre)
-    assert_equal 0.17, @potluck.ratio(:dessert)
+    assert_equal 33.3, @potluck.ratio(:entre)
+    assert_equal 16.7, @potluck.ratio(:dessert)
   end
 end
